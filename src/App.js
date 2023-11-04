dbcbf3c3c6886126170707facae41fb7984386de
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2023-11-04 21:53:46 +0000

    Implemented Routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import Home from './components/pages/Home/Home';
+import SingleTable from './components/pages/SingleTable/SingleTable';
+import NotFound from './components/pages/NotFound/NotFound';
+import { Routes, Route } from 'react-router-dom';
+
 const App = () => {
-	return <div></div>;
+	return (
+		<main>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path={'/table/:id'} element={<SingleTable />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+		</main>
+	);
 };
 
 export default App;
---
+++ src/components/pages/SingleTable/SingleTable.js
@@ -0,0 +1,9 @@
+const SingleTable = () => {
+	return (
+		<div>
+			<p>SingleTable...</p>
+		</div>
+	);
+};
+
+export default SingleTable;
