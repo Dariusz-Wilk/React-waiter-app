d611f6d2317613c8c127d2fc7b849a8a503ad535
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2023-11-05 15:18:46 +0000

    GET data from API by using Redux-Thunk

--- src/App.js
+++ src/App.js
@@ -5,8 +5,14 @@ import { Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/views/Header/Header';
 import Footer from './components/views/Footer/Footer';
+import { fetchTables } from './redux/tablesReducer';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 
 const App = () => {
+	const dispatch = useDispatch();
+
+	useEffect(() => dispatch(fetchTables()), [dispatch]);
 	return (
 		<Container>
 			<Header />
